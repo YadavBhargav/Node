21d1be6bd5ef46e739eaac5b3382d51c2ccd55f3
YadavBhargav <yadavbhargav.yb@gmail.com>
2023-04-23 11:10:25 +0530

    Crud complate

--- node-tut/index.js
+++ node-tut/index.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
+mongoose.connect("mongodb://127.0.0.1:27017/e-comm");
+const ProductSchema = new mongoose.Schema({
+    name: String,
+    price: Number,
+    brand: String,
+    category: String,
+});
 
-const main = async () => {
-    await mongoose.connect("mongodb://127.0.0.1:27017/e-comm");
-    const ProductSchema = new mongoose.Schema({
-        name: String,
-        price: Number,
-        brand: String,
-        category: String,
-    });
-
+const saveInDB = async () => {
     const ProductModel = mongoose.model('products', ProductSchema);
     let data = new ProductModel({
         name: "Narzo 55",
@@ -20,4 +19,30 @@ const main = async () => {
     console.log(result)
 }
 
-main()
+const updateInDB = async () => {
+    const ProductModel = mongoose.model('products', ProductSchema);
+    let data = await ProductModel.updateOne(
+        { name: "Narzo 55" },
+        {
+            $set: { price: "15000", name: "Narzo 10" }
+        }
+    )
+    console.log(data);
+}
+
+const deleteInDB = async () => {
+    const ProductModel = mongoose.model('products', ProductSchema);
+    let data = await ProductModel.deleteOne({ name: "Narzo 10" });
+    console.log(data);
+}
+
+
+const findInDB = async () => {
+    const ProductModel = mongoose.model('products', ProductSchema);
+    let data = await ProductModel.find();
+    console.log(data);
+}
+
+findInDB()
+
+
