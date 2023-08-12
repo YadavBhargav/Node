e7d70f147ce1bf8fc89f8e9bba45726eabe0466d
YadavBhargav <yadavbhargav.yb@gmail.com>
2023-08-12 12:13:47 +0530

    Event-emitter-returns-how-many-api-call

--- node-tut/index.js
+++ node-tut/index.js
@@ -1,7 +1,29 @@
-const os = require('os');
-// console.log(os.arch());
-// console.log(os.freemem()/(1024*1024*1024));
-// console.log(os.totalmem()/(1024*1024*1024));
-// console.log(os.hostname());
-// console.log(os.platform());
-console.log(os.userInfo());
+const express = require('express');
+const EventEmitter = require("events");
+const app = express();
+const event = new EventEmitter();
+
+/* 
+    EventEmitter returns How many api call at a time
+*/
+
+let count = 0;
+
+event.on("CountAPI", () => {
+    count++;
+    console.log("object", count);
+})
+
+app.get("/", (req, res) => {
+    res.send("api called")
+    event.emit("CountAPI")
+})
+
+app.get("/search", (req, res) => {
+    res.send("Search api called")
+})
+app.get("/update", (req, res) => {
+    res.send("update api called")
+})
+
+app.listen(5000);
