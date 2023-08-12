22db593aea0ca91ca609a8c4ee0d7a58b68cbc86
YadavBhargav <yadavbhargav.yb@gmail.com>
2023-08-12 11:30:46 +0530

    File upload api added in this code

--- node-tut/index.js
+++ node-tut/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const multer = require("multer");
 require("./config");
 
 const Product = require("./product");
@@ -6,23 +7,30 @@ const Product = require("./product");
 const app = express();
 app.use(express.json());
 
+/* Create api */
 app.post("/create", async (req, res) => {
     let data = new Product(req.body)
     let result = await data.save();
     res.send(result)
 })
+/* End */
 
+/* List api */
 app.get("/list", async (req, res) => {
     let data = await Product.find();
     res.send(data)
 })
+/* End */
 
+/* Delete by id api */
 app.delete("/delete/:_id", async (req, res) => {
     console.log(req.params)
     let data = await Product.deleteOne(req.params)
     res.send(data)
 })
+/* End */
 
+/* Update by id api */
 app.put("/update/:_id", async (req, res) => {
     console.log(req.params)
     let data = await Product.updateOne(
@@ -31,7 +39,9 @@ app.put("/update/:_id", async (req, res) => {
     );
     res.send(data)
 })
+/* End */
 
+/* Search api */
 app.get("/search/:key", async (req, res) => {
     console.log(req.params.key)
     let data = await Product.find(
@@ -45,5 +55,23 @@ app.get("/search/:key", async (req, res) => {
     )
     res.send(data)
 })
+/* End */
 
-app.listen(5000)
+/* File upload api */
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, "uploads")
+        },
+        filename: function (req, file, cb) {
+            cb(null, file.fieldname + "-" + Date.now() + ".jpg")
+        }
+    })
+}).single("user_file");
+
+app.post("/upload", upload, (req, res) => {
+    res.send("updaloded")
+})
+/* End */
+
+app.listen(5000);
