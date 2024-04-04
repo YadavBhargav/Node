666a28efbad25646e538437721561cf859f53eb7
YadavBhargav <yadavbhargav.yb@gmail.com>
2024-04-04 08:27:52 +0530

    Delete api done

--- project-1/backend/index.js
+++ project-1/backend/index.js
@@ -77,13 +77,18 @@ app.post("/addProduct", async (req, res) => {
     res.send(result)
 })
 
-app.get("/productsList", async (reqm, res) => {
+app.get("/productsList", async (req, res) => {
     let products = await Product.find();
     if (products.length > 0) {
         res.send(products);
     } else {
         res.send({ result: "No products found" });
     }
+})
+
+app.delete("/deleteProduct/:id", async (req, res) => {
+    const result = await Product.deleteOne({ _id: req.params.id })
+    res.send(result)
 })
 
 
