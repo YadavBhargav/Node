d1dbd25a26fea6ff009cd0fb9b98d0374a70c878
YadavBhargav <yadavbhargav.yb@gmail.com>
2024-04-18 08:43:13 +0530

    cluster related changes

--- project-1/backend/db/config.js
+++ project-1/backend/db/config.js
@@ -1,2 +1,12 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://0.0.0.0:27017/e-commerce')
+const url = `mongodb+srv://yadavbhargavyb:<2JlonzzoybQ7QoOy>@bmcluster.urz6qks.mongodb.net/`
+const connectionOptions = { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false };
+mongoose.connect(url, connectionOptions)
+    .then(() => {
+        console.log('Connected to the database ')
+    })
+    .catch((err) => {
+        console.error(`Error connecting to the database. n${err}`);
+    })
+    
+// mongoose.connect('mongodb://0.0.0.0:27017/e-commerce')
